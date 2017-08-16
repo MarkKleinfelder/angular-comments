f5a10c93781b0357470488805363b808436a8458
Mark <MarkKleinfelder@gmail.com>
2017-08-15 18:08:06 -0600

    bulletpoints done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,9 +10,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  comments = [
-      'first comment!',
-      'nice work!',
-      'I would also like to congratulate you!'
-  ];
+
+comments = [];
+
+  // comments = [{
+  //   text: 'first comment!',
+  // 	author: 'A. Person'
+  // 	}
+      // 'nice work!',
+      // 'I would also like to congratulate you!'
+  //];
+
+commentText = '';
+authorText = '';
+comment = {
+  text:'',
+  author: ''
 }
+
+  submitComment(){
+    console.log(this.authorText);
+    console.log(this.commentText);
+    this.comment = {
+      text: this.commentText,
+      author: this.authorText
+    }
+
+    console.log(this.comment)
+    this.comments.push(this.comment)
+  }
+
+
+  deleteComment(i){
+    console.log('delete function hit')
+    this.comments.splice(i, 1)
+}
+
+  updateComment(i){
+    console.log('update function hit!');
+    this.comment = {
+      text:this.commentText,
+      author:this.authorText
+    }
+    this.comments.splice(i,1,this.comment)
+   
+  }
+  //   
+  // }
+  // values = '';
+
+  // commentText(event: any) {
+  //   this.values += event.target.value + ' | ';
+  //   console.log(this.values)
+  // }
+
+
+};
